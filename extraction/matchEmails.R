3c104ceb5ac5b91dce30d2641e77a1c437e65fc5
IreneSophia <10planki@gmail.com>
2025-10-27 11:54:40 +0100

    Improved matching of emails, but still threshold etc. missing

--- extraction/matchEmails.R
+++ extraction/matchEmails.R
@@ -9,48 +9,60 @@ df.authors = read_csv("authorList_ext.csv")
 df.sel = df.authors %>% 
   group_by(id) %>% mutate(n = sum(!is.na(email))) %>%
   ungroup() %>%
-  filter(pdf.email != "false" & n > 0 & !is.na(id)) %>%
-  select(id, pdf.email, name.surname, name.given) %>% 
+  filter(pdf.all.emails != "false" & n > 0 & !is.na(id)) %>%
+  select(id, email, pdf.all.emails, name.surname, name.given) %>% 
   mutate(email.possible = NA, email.similarity = NA)
 
 ls.ids = split(df.sel, df.sel$id)
 
 for (i in 1:length(ls.ids)) { #
   preprint = names(ls.ids)[i]
-  emails = str_split(ls.ids[[i]]$pdf.email[1], "\", \"")
+  emails = str_split(ls.ids[[i]]$pdf.all.emails[1], "\", \"")
   for (k in 1:length(ls.ids[[i]]$name.surname)) {
     similar = c()
     for (j in 1:length(emails[[1]])) {
       # getting rid of the @... for each of the email
-      email = gsub("@.*", "", tolower(emails[[1]][j]))
-      similar = c(similar, levenshteinSim(tolower(ls.ids[[i]]$name.surname[k]),
-                              email))
+      email.sel = gsub("@.*", "", tolower(emails[[1]][j]))
+      # add the highest similarity value for this email / person combination
+      similar = c(similar, max(
+        # similarity with surname
+        levenshteinSim(tolower(ls.ids[[i]]$name.surname[k]),
+                       email.sel),
+        # similarity with given name
+        levenshteinSim(tolower(ls.ids[[i]]$name.given[k]),
+                       email.sel),
+        # similarity with full name
+        levenshteinSim(tolower(sprintf('%s %s', 
+                                       ls.ids[[i]]$name.given[k],
+                                       ls.ids[[i]]$name.surname[k])),
+                       email.sel),
+        levenshteinSim(tolower(sprintf('%s %s', 
+                                       ls.ids[[i]]$name.surname[k],
+                                       ls.ids[[i]]$name.given[k])),
+                       email.sel), na.rm = T))
+      
     }
-    idx = which(similar == max(similar))
+    idx = which(similar == max(similar, na.rm = T))
     if (length(idx) > 1) idx = idx[1]
-    if (sum(similar) > 0) {
-      r = which(df.sel$id == preprint & df.sel$name.surname == ls.ids[[i]]$name.surname[k] & df.sel$name.given == ls.ids[[i]]$name.given[k])
-      df.sel[r,]$email.possible   = emails[[1]][idx]
-      df.sel[r,]$email.similarity = similar[idx]
-    }
+    r = which(df.sel$id == preprint & df.sel$name.surname == ls.ids[[i]]$name.surname[k] & df.sel$name.given == ls.ids[[i]]$name.given[k])
+    df.sel[r,]$email.possible   = emails[[1]][idx]
+    df.sel[r,]$email.similarity = similar[idx]
   }
 }
 
-# get rid of quotation marks
-df.sel = df.sel %>%
-  mutate(
-    email.possible = gsub("\"", "", email.possible),
-    # adjust the similarity for the length ratio
-    email.similarity.adj = email.similarity * (nchar(name.surname)/nchar(gsub("@.*", "", tolower(email.possible))))
-  )
 
-# only keep best match for each email + preprint
-df.test = df.sel %>%
+df.email = df.sel %>%
+  mutate(
+    # get rid of quotation marks
+    email.possible = gsub("\"", "", email.possible)
+  ) %>%
+  # figure out best match per preprint and possible email
   group_by(id, email.possible) %>%
   mutate(email.rank = max(rank(email.similarity)) - rank(email.similarity)) %>%
-  filter(email.rank == 0)
+  # only keep the best fit and where we don't have an email yet
+  filter(email.rank == 0 & is.na(email))
 
-# figure out a threshold that makes sense, either for adjusted or not
+# figure out a threshold that makes sense, possibly adjusted for length ratio
 
 # there are some that don't fit despite really good similariy, e.g.:
 # Melacarne Claudio is fit to elcra@unisi.it > probably not a fit? maybe? how do we tell?
